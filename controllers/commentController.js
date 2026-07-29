c792366902ae00ba5bdf9565dbaf86d00234f69d
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2026-07-30 03:24:10 +0500

    create delete comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -63,3 +63,27 @@ exports.getCommentsByPost = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+// ---------------- DELETE COMMENT ----------------
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = await Comment.findByPk(id);
+
+    if (!comment) {
+      return res.status(404).json({ message: "There is no comment" });
+    }
+
+    if (comment.userId !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "You cant't delete this commnent" });
+    }
+
+    await comment.destroy();
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
